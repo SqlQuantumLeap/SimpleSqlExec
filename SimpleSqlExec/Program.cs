bb3fcd7c220ddeb71bb2163b5227cb238eeb8f73
Sql Quantum Leap <SqlQuantumLeap@users.noreply.github.com>
2015-11-06 11:48:53 -0500

    Initial setup
    Add new project files.

---
+++ SimpleSqlExec/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+namespace SimpleSqlExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
